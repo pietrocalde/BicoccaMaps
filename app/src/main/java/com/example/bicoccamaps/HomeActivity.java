82af19e8767cbf84fa855e0d0312e4ecd37ee5e1
FabioGabri <Loportoio01>
2022-12-08 15:45:26 +0100

    search bar grafica

--- app/src/main/java/com/example/bicoccamaps/HomeActivity.java
+++ app/src/main/java/com/example/bicoccamaps/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.example.bicoccamaps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,7 +16,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         // Initialize fragment
         Fragment fragment = new MapFragment();
         // Open fragment
@@ -19,5 +24,29 @@ public class HomeActivity extends AppCompatActivity {
                 .beginTransaction().replace(R.id.frame_layout,fragment)
                 .commit();
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+
+        MenuItem.OnActionExpandListener onActionExpandListener= new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem menuItem) {
+                Toast.makeText(HomeActivity.this,"Search si è aperto", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem menuItem) {
+                Toast.makeText(HomeActivity.this,"Search si è chiuso", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        };
+        menu.findItem(R.id.search).setOnActionExpandListener(onActionExpandListener);
+        SearchView searchView=(SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setQueryHint("Cerca qui...");
+        Toast.makeText(HomeActivity.this,"Search si è aperto", Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
