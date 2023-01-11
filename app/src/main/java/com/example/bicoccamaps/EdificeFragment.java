d3b7aa26043cc75aabbd611a286cdab9d725c8ce
Calde <pietrocalde@yahoo.it>
2023-01-11 12:35:05 +0100

    configurazione firebase

--- app/src/main/java/com/example/bicoccamaps/EdificeFragment.java
+++ app/src/main/java/com/example/bicoccamaps/EdificeFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DatabaseReference;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +22,7 @@ public class EdificeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
