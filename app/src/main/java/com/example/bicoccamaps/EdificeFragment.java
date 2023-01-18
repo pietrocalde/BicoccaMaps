bf992fc26d290b4ce04456f65c02b4192fa0542b
FabioGabri <Loportoio01>
2023-01-18 23:00:25 +0100

    debug

--- app/src/main/java/com/example/bicoccamaps/EdificeFragment.java
+++ app/src/main/java/com/example/bicoccamaps/EdificeFragment.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.firebase.database.DatabaseReference;
 
 /**
  * A simple {@link Fragment} subclass.
